fede33a22809d71aa862c85a30c97be35ee31845
Joey Grable <hello@joeygrable.com>
2026-03-27 11:42:39 -0700

    Revert "Revert "fix: render solidjs components""
    This reverts commit eb6b21d1b96ea74762bfd54abc74f5a0b55d33dc.

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,6 +1,5 @@
-import { solidPlugin } from "esbuild-plugin-solid"
-import type { UserConfig } from "tsdown"
 import { defineConfig } from "tsdown"
+import solid from "unplugin-solid/rolldown"
 
 // export default defineConfig({
 //   entry: ["src/index.ts"],
@@ -11,19 +10,9 @@ import { defineConfig } from "tsdown"
 //   treeshake: true
 // })
 
-function generateConfig(jsx: boolean): UserConfig {
-  return {
-    target: "esnext",
-    platform: "browser",
-    format: "esm",
-    clean: true,
-    dts: !jsx,
-    entry: ["src/index.ts"],
-    outDir: "dist/",
-    treeshake: true,
-    sourcemap: true,
-    plugins: !jsx ? [solidPlugin({ solid: { generate: "dom" } })] : []
-  }
-}
-
-export default defineConfig([generateConfig(false), generateConfig(true)])
+export default defineConfig({
+  entry: ["./src/index.ts"],
+  platform: "neutral",
+  dts: true,
+  plugins: [solid()]
+})
